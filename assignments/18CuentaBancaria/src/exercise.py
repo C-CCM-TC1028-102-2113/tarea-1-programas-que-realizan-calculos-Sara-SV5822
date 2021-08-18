383663a98efc0bd1a2e0fddc834e848a251d1b73
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 15:45:36 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    saldo_a=float(input('Dame el saldo del mes anterior: '))
+    ingresos= float(input('Dame los ingresos: '))
+    egresos= float(input('Dame los egresos: '))
+    num=int(input('Dame el número de cheques: '))
+
+    saldo_f= saldo_a-egresos+ingresos
+    total= saldo_f-(saldo_f*0.075)-(num*13)
+
+    print('El saldo final de la cuenta es: '+ str(total))
+
     pass
 
 if __name__ == '__main__':
