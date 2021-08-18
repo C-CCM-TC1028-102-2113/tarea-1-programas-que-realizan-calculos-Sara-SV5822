959dc8b9d750cfea4cf76da261bc3a6b830ddef9
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 18:53:59 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,11 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    time= float(input('Dame los minutos: '))
+    time=float(input('Dame los minutos:'))
 
-    cen= float((5.7*60/10)*time)
+    cen= round((5.7*60/10)*time,1)
 
-    print('Centímetros recorridos: '+ str(round(float(cen),1)))
+    print('Centímetros recorridos: '+ str(cen))
 
     pass
 
