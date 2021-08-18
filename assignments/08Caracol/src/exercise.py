df979ab376d1d392fe8ab001aeb44784268e2d94
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 15:50:35 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,9 +2,10 @@ def main():
     #escribe tu código abajo de esta línea
     min=float(input('Dame los minutos: '))
 
-    cm=float(min*34.2)
+    cm=((5.7*60)/100)*min
 
     print('Centímetros recorridos: '+ str(cm))
+
     pass
 
 if __name__ == '__main__':
