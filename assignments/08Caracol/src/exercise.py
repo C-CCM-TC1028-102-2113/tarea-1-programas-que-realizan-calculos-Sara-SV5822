7724d682403f5b84c1b2c3ecaccbee584e699d11
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 15:34:14 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,10 +1,11 @@
 def main():
-  #escribe tu código abajo de esta línea
-  min=float(input('Dame los minutos: '))
+    #escribe tu código abajo de esta línea
+    min=float(input('Dame los minutos: '))
 
-  cm=float(min*34.2)
+    cm=float(min*34.2)
 
-  print('Centímetros recorridos: '+ str(cm))
+    print('Centímetros recorridos: '+ str(cm))
+    pass
 
 if __name__ == '__main__':
     main()
