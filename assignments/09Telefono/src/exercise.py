115fadf50f0b9d8588d42958ea723a54c795c648
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 15:23:41 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,8 +1,12 @@
 def main():
-    #escribe tu código abajo de esta línea
-    #Leer los datos
-    pass
+  #escribe tu código abajo de esta línea
+  msj=int(input('Dame el número de mensajes: '))
+  megas=float(input('Dame el número de megas: '))
+  min=int(input('Dame el número de minutos: '))
 
+  cost= float((msj+megas+min)*0.80)
+
+  print('El costo mensual es: '+ str(cost))
 
 if __name__ == '__main__':
     main()
