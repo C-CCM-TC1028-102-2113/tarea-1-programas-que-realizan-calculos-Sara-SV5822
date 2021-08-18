6ec232c88eb7ce3b37446437f82431cb4c0ed348
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 16:46:06 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pag=int(input('Dame el número de palabras: '))
 
+    if ((pag % 475) == 0):
+        pag=pag/475
+        costo= float((pag*60)*.9)
+        print('El costo de la publicación es: '+ str(costo))
+    
+
+
+    else:
+        pag= (pag//475)+1
+        costo= float((pag*60)*.9)
+        print('El costo de la publicación es: '+ str(costo))
+
+    pass
 
 if __name__ == '__main__':
     main()
