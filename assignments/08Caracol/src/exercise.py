d39be20d4db8a53695cd73afe4482feb79869329
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 15:15:41 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,7 +2,7 @@ def main():
   #escribe tu código abajo de esta línea
   min=float(input('Dame los minutos: '))
 
-  cm=float(min*0.57*60)
+  cm=round(float(min*0.57*60),1)
 
   print('Centímetros recorridos: '+ str(cm))
 
